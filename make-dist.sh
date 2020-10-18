5d5c54b5028bd75817a4656793f641e57c6ee43b
Sławomir Śledź <slawomir.sledz@iiit.pl>
2020-10-18 16:56:40 +0200

    improving dist script

--- make-dist.sh
+++ make-dist.sh
@@ -1,2 +1,4 @@
-cat jad.sh target/oolong-1.0-SNAPSHOT-jar-with-dependencies.jar > target/jad
+set -x
+version=$(mvn -o org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '^\[')
+cat jad.sh "target/oolong-$version-jar-with-dependencies.jar" > target/jad
 chmod +x target/jad
